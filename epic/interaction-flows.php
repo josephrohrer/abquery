8d8ce960437c66b239e29a0a85d06a1ccc9b4480
Joseph Rohrer <josephrohrer@me.com>
2017-01-25 15:37:36 -0700

    Updated interaction flow for Burt.

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -6,12 +6,12 @@
 	</head>
 	<body>
 		<h3>Interaction flow for Tina Jenkins</h3>
-		<ol>
+		<ul>
 			<li>Tina decides to look at our site on her laptop to look up houses that she can potentially move to.</li>
 			<li>The site displays a miniature version of the map that can be clicked to expand into a full version that will be coated in heatmap layers. And there will be a search option where they can input an address to search an area directly.</li>
 			<li>Since she has a list of potential addresses she inputs one into the search bar.</li>
 			<li>The site will send back cards with information on crime and parks which can be toggled or expanded to see the metadata.</li>
-		</ol>
+		</ul>
 		<h3>Interaction Flow for Steven Smtiherton</h3>
 		<ul>
 			<li>Stephen types his potential address into the search field</li>
@@ -20,6 +20,15 @@
 			<li>The site displays a closer view</li>
 			<li>Stephen taps on a park nearest to the address</li>
 			<li>The site displays further info about the park</li>
+		</ul>
+		<h3>Interaction Flow for Burt Macklin</h3>
+		<ul>
+			<li>Burt types into a Google search: "Safe place for a business in Albuquerque."</li>
+			<li>"ABQ Move Safe" shows up on the first page of search results and Burt clicks on it.</li>
+			<li>Burt enters in one of the potential addresses for his business into the search bar and presses enter.</li>
+			<li>The site displays a map with his potential business in the center and a default view of crime surrounding it.</li>
+			<li>He is able to change the default view to expand the area of his search and to filter the various crimes.</li>
+			<li></li>
 		</ul>
 	</body>
 
