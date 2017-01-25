daf3b8a68de913c93f767d20d4b5280fd3d49e6d
josephrohrer <josephrohrer@me.com>
2017-01-25 12:53:30 -0700

    Create index.php

---
+++ epic/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Team safeHouse</title>
+	</head>
+	<body>
+		<header>
+			<h1>Team Safe House</h1>
+		</header>
+		<main>
+			<h2>Executive Summary</h2>
+				<p>Desktop and mobile website for home buyers and home owners in Albuquerque, New Mexico. Users are able to research both positive and negative attributes of the neighborhood they are living in/ potentially moving to. After inputting an address,  information about proximity to public parks and/or various criminal activity will be displayed. The user can select features to filter specifics such as the number of miles surrounding the address searched, etc. Data returned will be displayed in a map as well as.... 		</p>
+			<h2>User Stories</h2>
+				<ol>
+					<li>As a user, I want to know what the crime rate is in a neighborhood I am potentially buying a home.</li>
+					<li>As a user, I want to know what types of crimes are committed in the neighborhood I currently live in.</li>
+					<li>As a user, I want to know where parks are at in a neighborhood I am potentially buying a home.</li>
+					<li>As a user, I want to know where parks are at in the neighborhood I currently live in.</li>
+				</ol>
+			<h2>Personas</h2>
+				<ul>
+					<li><a href="burt-macklin.php">Burt Macklin</a></li>
+					<li><a href="tina-jenkins.php">Tina Jenkins</a></li>
+				</ul>
+					<h2><a href="use-cases.php">Use Cases</a></h2>
+					<h2><a href="interaction-flows.php">Interaction Flows</a></h2>
+</main>
+</body>
+</html>
