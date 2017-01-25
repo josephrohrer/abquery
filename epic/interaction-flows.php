98ae8cb05d9c964ad244ad0ffe57abafdd51a60a
Ben Smith <rios7467@gmail.com>
2017-01-25 13:06:46 -0700

    Added html sytnax for interaction flow

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Interaction flows for the personas.</title>
+	</head>
+	<body>
+		<h3>Interaction flow for Tina Jenkins</h3>
+	</body>
 
+</html>
