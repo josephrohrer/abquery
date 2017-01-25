e3b827d4050c80ffb5fadc88ca0592328217598f
Brett Gilbert <bgilb13@gmail.com>
2017-01-25 14:39:03 -0700

    added interaction flow 2

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -10,6 +10,15 @@
 			<li>Tina decides to look at our site on her laptop to look up houses that she can potentially move to in the area. She already has a few addresses that she can input into the search bar to bring up the information on the recent crime committed in the area and the information on parks in the area.</li>
 			<li>The site will send back cards with information on crime and parks respectively which can be toggled or expanded to see the metadata.</li>
 		</ol>
+		<h3>Interaction Flow for Steven Smtiherton</h3>
+		<ul>
+			<li>Stephen types his potential address into the search field</li>
+			<li>The site returns a map centered upon the address displaying crime and parks in the area</li>
+			<li>Stephen zooms in to more closely view the neighborhood</li>
+			<li>The site displays a closer view</li>
+			<li>Stephen taps on a park nearest to the address</li>
+			<li>The site displays further info about the park</li>
+		</ul>
 	</body>
 
 </html>
