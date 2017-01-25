933a71b4f3f4c848b6c8e7fbb56e0856c4c0afbd
Brett Gilbert <bgilb13@gmail.com>
2017-01-25 13:21:33 -0700

    added steven-smtiherton.php

---
+++ epic/steven-smtiherton.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Steven Smtiherton</title>
+	</head>
+	<body>
+		<h1>Steven Smtiherton Persona and Use Case</h1>
+		<h2>Persona</h2>
+		<p>Name: Steven Smtiherton</p>
+		<p>Age: 37</p>
+		<p>Profession: Professional kettlebell strength trainer</p>
+		<p>Technology: Droid Turbo 2 running Android Marshmallow, Macbook Air running OS X</p>
+		<p>Needs: As a professional strength trainer and gym rat, Steven has little time, patience, and mental capacity to do research of any type. If it cant be done on his phone during his commute, it doesn't get done. Steven needs a quick, easy to read method for figuring out if a prospective house is in a good neighborhood with parks.</p>
+		<p>Goals: Steven's goal is to determine whether a perspective house is in a safe neighborhood or not. The easier this research can be for him, the better.</p>
+		<h2>Use Case</h2>
+		<p>Steven has recently moved to Albuquerque for a job opening at GLoboGym and has been living in an apartment. He likes his apartment, but feels that it is a bit too far from the gym for his liking and wishes it had a larger yard gor his dog, Protein. On his way to the gym, Steven notices a house that has everything he wants, fits his price range, and is within a weight vest walk to the gym. Because the safety of his dog and steroids are of the utmost importance, Steven decides to check if this house is in a safe neighborhood. He needs a quick, easy to read way to check out the neighborhood and see if there are any parks around to take Protein to. While weaving through traffic dangerously, he pulls out his phone. He types in the URL, enters the address of the house he's interested in, and is delighted to find that the neighborhood is very safe and that there are 2 parks within a mile. Satisfied with how quick he was able to get the information he needed, Steven decides to pay attention to the road between chugs of his sugar free monster.</p>
+	</body>
+</html>
