6d408c657c0f24e9407900933423e2615bd3c652
Jennifer Minnich <jminniecc@gmail.com>
2017-01-25 13:41:38 -0700

    Commit index.php / updated persona links

--- epic/index.php
+++ epic/index.php
@@ -2,15 +2,15 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Team safeHouse</title>
+		<title>ABQ moveSafe</title>
 	</head>
 	<body>
 		<header>
-			<h1>Team Safe House</h1>
+			<h1>ABQ moveSafe</h1>
 		</header>
 		<main>
 			<h2>Executive Summary</h2>
-				<p>Desktop and mobile website for home buyers and home owners in Albuquerque, New Mexico. Users are able to research both positive and negative attributes of the neighborhood they are living in/ potentially moving to. After inputting an address,  information about proximity to public parks and/or various criminal activity will be displayed. The user can select features to filter specifics such as the number of miles surrounding the address searched, etc. Data returned will be displayed in a map as well as.... 		</p>
+				<p>Desktop and mobile website for home buyers and home owners in Albuquerque, New Mexico. Users are able to research both positive and negative attributes of the neighborhood they are living in/ potentially moving to. After inputting an address,  information about proximity to public parks and/or various criminal activity will be displayed. The user can select features to filter specifics such as the number of miles surrounding the address searched, etc. Data returned will be displayed in an option box listing crimes or parks that can be selected (see wireframe for visuals), whatever box is selected can then be viewed in a map with a rating system.</p>
 			<h2>User Stories</h2>
 				<ol>
 					<li>As a user, I want to know what the crime rate is in a neighborhood I am potentially buying a home.</li>
@@ -21,6 +21,7 @@
 			<h2>Personas</h2>
 				<ul>
 					<li><a href="burt-macklin.php">Burt Macklin</a></li>
+					<li><a href="steven-smtiherton.php">Steven-Smitherton</a></li>
 					<li><a href="tina-jenkins.php">Tina Jenkins</a></li>
 				</ul>
 					<h2><a href="use-cases.php">Use Cases</a></h2>
