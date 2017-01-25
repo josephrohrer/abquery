2c158a93cde7e27236d4052e8111ca37f821e5b4
Ben Smith <rios7467@gmail.com>
2017-01-25 15:01:34 -0700

    Changed the interaction flow again for Tina to match Bretts

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -7,8 +7,10 @@
 	<body>
 		<h3>Interaction flow for Tina Jenkins</h3>
 		<ol>
-			<li>Tina decides to look at our site on her laptop to look up houses that she can potentially move to in the area. She already has a few addresses that she can input into the search bar to bring up the information on the recent crime committed in the area and the information on parks in the area.</li>
-			<li>The site will send back cards with information on crime and parks respectively which can be toggled or expanded to see the metadata.</li>
+			<li>Tina decides to look at our site on her laptop to look up houses that she can potentially move to.</li>
+			<li>The site displays a miniature version of the map that can be clicked to expand into a full version that will be coated in heatmap layers. And there will be a search option where they can input an address to search an area directly.</li>
+			<li>Since she has a list of potential addresses she inputs one into the search bar.</li>
+			<li>The site will send back cards with information on crime and parks which can be toggled or expanded to see the metadata.</li>
 		</ol>
 		<h3>Interaction Flow for Steven Smtiherton</h3>
 		<ul>
