264dd43b58e4230776687a882f14c7fb8ff449a2
josephrohrer <josephrohrer@me.com>
2017-01-25 12:01:41 -0700

    Create burt-macklin.php

---
+++ epic/burt-macklin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona of Burt Macklin</title>
+	</head>
+	<body>
+		<h4>Persona of Burt Macklin</h4>
+		<ul>
+			<li>Name: Burt Macklin</li>
+			<li>Age: 45</li>
+			<li>Occupation: Car Restorer</li>
+			<li>Technology: Alcatel Onetouch Retro (Yes, that's a flip phone) and an 8-year-old Dell laptop running Windows XP</li>
+			<li>Needs: </li>
+			<li>Goals: Glendas goals are to try and keep her daughters love and to be surrounded by apple products constantly.</li>
+		</ul>
+		<h4>Use case for Burt Macklin</h4>
+		<ol>
+			<li>Glenda is at work in the afternoon at about 16:00 when she realizes that she forgot to get her daughter a present for her birthday tomorrow so she pulls out her trust iPhone and opens safari to see what she can find.</li>
+			<li>She has to look into local places since there is no time to have something shipped and finds that Balloon Cocoons is a local business that sells quirky balloons stuffed with teddy bears, candy, flowers, etc.</li>
+			<li>She sees that they have a website and clicks the link.</li>
+			<li>There are a few options that the website pulls up for her like "premade balloons" and "custom orders"</li>
+			<li>She clicks "premade balloons" and the page opens up into a new list with pictures and prices of each balloon.</li>
+			<li>She finds the balloon she likes and clicks on "order now" listed underneath the photo which brings the site to a page saying she can pick up the balloon whenever she is ready with a field to put in the timeframe she should be able to pick up the balloon.</li>
+			<li>The website sends my mom an email letting her know the type of balloon and what time she will be there to pick it up.</li>
+			<li>After work she drives to pick up the balloon and pays with a check.</li>
+			<li>The next day her daughter is excited to receive her present.</li>
+		</ol>
+	</body>
