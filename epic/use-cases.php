3adc3383a8d849b93ce100133d986bdee878ac2e
Brett Gilbert <bgilb13@gmail.com>
2017-01-25 14:50:58 -0700

    put use case in use-cases.php

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Use Cases</title>
+	</head>
+	<body>
+		<h1>Persona Use Cases</h1>
+		<h2>Tina Jenkins</h2>
+		<h2>Steven Smtiherton</h2>
+		<p>Steven has recently moved to Albuquerque for a job opening at GLoboGym and has been living in an apartment. He likes his apartment, but feels that it is a bit too far from the gym for his liking and wishes it had a larger yard gor his dog, Protein. On his way to the gym, Steven notices a house that has everything he wants, fits his price range, and is within a weight vest walk to the gym. Because the safety of his dog and steroids are of the utmost importance, Steven decides to check if this house is in a safe neighborhood. He needs a quick, easy to read way to check out the neighborhood and see if there are any parks around to take Protein to. While weaving through traffic dangerously, he pulls out his phone. He types in the URL, enters the address of the house he's interested in, and is delighted to find that the neighborhood is very safe and that there are 2 parks within a mile. Satisfied with how quick he was able to get the information he needed, Steven decides to pay attention to the road between chugs of his sugar free monster.</p>
+		<h2>Burt Macklin</h2>
+	</body>
+</html>
