c03a3cdfcf96e1e7ad7814952be982357a317859
Ben Smith <rios7467@gmail.com>
2017-01-25 13:29:56 -0700

    Added tinas interaction flow in its entirety.

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -6,6 +6,12 @@
 	</head>
 	<body>
 		<h3>Interaction flow for Tina Jenkins</h3>
+		<ol>
+			<li>Tina decides to look at our site on her laptop to look up houses that she can potentially move to in the area. The site sends back a topographical map with overlapped heatmaps of crime and parks.</li>
+			<li>She sees that there is an option to look up houses by address and she has a few onhand that she was already looking at. The site opens a text box to type in an address.</li>
+			<li>She types the address and then it pulls a rating based on the recent crimes in the area cross referenced on whether or not there is a park nearby, with a drop down menu that has radii that can be set for that particular area and an option to actually list the crimes and times they were committed as well as distance from the location that was pinged.</li>
+			<li>She can then determine if she is interested in the house based on the information she is given.</li>
+		</ol>
 	</body>
 
 </html>
