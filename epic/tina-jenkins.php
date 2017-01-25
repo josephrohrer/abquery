d210089f1cb154fef59229c0e73acdce2db3e7de
josephrohrer <josephrohrer@me.com>
2017-01-25 12:52:28 -0700

    Create tina-jenkins.php

---
+++ epic/tina-jenkins.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona for Tina Jenkens</title>
+	</head>
+	<body>
+		<h4>Persona of Tina Jenkens</h4>
+		<ul>
+			<li>Name: Tina Jenkens</li>
+			<li>Age: 37</li>
+			<li>Occupation: Stay at home mom.</li>
+			<li>Technology: iPhone 7 updated to iOS 10, with an older HP Laptop using windows 7 ultimate.</li>
+			<li>Needs: Needs to find a new house for her family.</li>
+			<li>Goals: Glendas goals are to try and keep her daughters love and to be surrounded by apple products constantly.</li>
+		</ul>
+	</body>
+</html>
