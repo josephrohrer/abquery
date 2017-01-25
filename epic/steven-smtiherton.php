d430964f8ad6e4adbfc48cef1860788a9e546161
Brett Gilbert <bgilb13@gmail.com>
2017-01-25 13:44:59 -0700

    content edit

--- epic/steven-smtiherton.php
+++ epic/steven-smtiherton.php
@@ -9,7 +9,7 @@
 		<h2>Persona</h2>
 		<p>Name: Steven Smtiherton</p>
 		<p>Age: 37</p>
-		<p>Profession: Professional kettlebell strength trainer</p>
+		<p>Occupation: Professional kettlebell strength trainer</p>
 		<p>Technology: Droid Turbo 2 running Android Marshmallow, Macbook Air running OS X</p>
 		<p>Needs: As a professional strength trainer and gym rat, Steven has little time, patience, and mental capacity to do research of any type. If it cant be done on his phone during his commute, it doesn't get done. Steven needs a quick, easy to read method for figuring out if a prospective house is in a good neighborhood with parks.</p>
 		<p>Goals: Steven's goal is to determine whether a perspective house is in a safe neighborhood or not. The easier this research can be for him, the better.</p>
