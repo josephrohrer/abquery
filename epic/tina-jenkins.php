960c7c41df78a90d4e3bee10d483a7bf4c4f3298
Ben Smith <rios7467@gmail.com>
2017-01-26 15:26:50 -0700

    Small tweak to tine jenkins interaction flow

--- epic/tina-jenkins.php
+++ epic/tina-jenkins.php
@@ -12,7 +12,7 @@
 			<li>Occupation: Stay at home mom.</li>
 			<li>Technology: iPhone 7 updated to iOS 10, with an older HP Laptop using windows 7 ultimate.</li>
 			<li>Needs: Tina needs to micromanage her family out of pure paranoia so she is trying to see how safe the neighborhood that shes currently living in.</li>
-			<li>Goals: Tinas goal is to find a way to determine if the house she is in is safe for her children by seeing what kind of crime data is available.</li>
+			<li>Goals: Tinas goal is to find a way to determine if the house she is in is safe for her children by seeing what kind of crime data is available, specifically breaking and entering and vehicular manslaughter.</li>
 		</ul>
 	</body>
 </html>
