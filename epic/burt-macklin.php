f0398db383a84f723bfce440c7cbb6e8dfe287c5
Joseph Rohrer <josephrohrer@me.com>
2017-01-26 13:27:03 -0700

    Updated Burt Macklin persona with Vista and more specifics about crime.

--- epic/burt-macklin.php
+++ epic/burt-macklin.php
@@ -10,8 +10,8 @@
 			<li>Name: Burt Macklin</li>
 			<li>Age: 45</li>
 			<li>Occupation: Car Restorer</li>
-			<li>Technology: Alcatel Onetouch Retro (Yes, that's a flip phone) and an 8-year-old Dell laptop running Windows XP</li>
+			<li>Technology: Alcatel Onetouch Retro (Yes, that's a flip phone) and an 8-year-old Dell laptop running Windows Vista.</li>
 			<li>Needs: Burt runs a car restoration business out of his home garage. His neighbors dislike the amount of cars that are kept around his property and he's gotten to the point where he needs to move his business from his home to a new business property. He is needing to find a place in Albuquerque that will not only accommodate his business but that he does not have to worry about being surrounded by crime to protect his customer's cars.</li>
-			<li>Goals: Burt would like to find a safe place in town to move his business to. He has an idea of a couple of places from his business realtor but would like to get an idea of the crime that surrounding businesses have potentially dealt with in the past. He would like a website that he is able to view crime based on an address that he types in. </li>
+			<li>Goals: Burt would like to find a safe place in town to move his business to. He has an idea of a couple of places from his business realtor but would like to get an idea of the crime that surrounding businesses have potentially dealt with in the past. He would like a website that he is able to view crime based on an address that he types in and would also like to view specifics about amount of stolen vehicles and vandalism in a given area. </li>
 		</ul>
 	</body>
