e91c83250393cb9ab4bdc4356bbb907e73214152
Jennifer Minnich <jminniecc@gmail.com>
2017-01-26 15:59:28 -0700

    Commit index.php / updated persona links

--- epic/index.php
+++ epic/index.php
@@ -10,10 +10,11 @@
 		</header>
 		<main>
 			<h2>Executive Summary</h2>
-				<p>Desktop and mobile website for home buyers and home owners in Albuquerque, New Mexico. Users are able to research both positive and negative attributes of the neighborhood they are living in/ potentially moving to. After inputting an address,  information about proximity to public parks and/or various criminal activity will be displayed. The user can select features to filter specifics such as the number of miles surrounding the address searched, etc. Data returned will be displayed in an option box listing crimes or parks that can be selected (see wireframe for visuals), whatever box is selected can then be viewed in a map with a rating system.</p>
+				<p>Desktop and mobile website for home buyers and home owners in Albuquerque, New Mexico. Users are able to research both positive and negative attributes of the neighborhood they are living in/ potentially moving to. After inputting an address, information about proximity to public parks and/or various criminal activity will be displayed. Potentially, data returned will be displayed in an option box listing crimes or parks that can be selected (see wireframe for visuals), whatever box is selected can then be viewed in a map with a rating system.</p>
 			<h2>User Stories</h2>
 				<ol>
 					<li>As a user, I want to know what the crime rate is in a neighborhood I am potentially buying a home.</li>
+					<li>As a user, I want to know what the crime rates is in a neighborhood I am potentially buying a business.</li>
 					<li>As a user, I want to know what types of crimes are committed in the neighborhood I currently live in.</li>
 					<li>As a user, I want to know where parks are at in a neighborhood I am potentially buying a home.</li>
 					<li>As a user, I want to know where parks are at in the neighborhood I currently live in.</li>
@@ -21,7 +22,7 @@
 			<h2>Personas</h2>
 				<ul>
 					<li><a href="burt-macklin.php">Burt Macklin</a></li>
-					<li><a href="steven-smtiherton.php">Steven-Smitherton</a></li>
+					<li><a href="steven-smtiherton.php">Steven Smitherton</a></li>
 					<li><a href="tina-jenkins.php">Tina Jenkins</a></li>
 				</ul>
 					<h2><a href="use-cases.php">Use Cases</a></h2>
