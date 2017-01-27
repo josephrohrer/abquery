92eaecbf960229fd82e986b2ec140b2872921893
Brett Gilbert <bgilb13@gmail.com>
2017-01-27 12:18:13 -0700

    added DDL to abq-move-safe.sql https://app.asana.com/0/256424297438821/256424297438825

--- sql/abq-move-safe.sql
+++ sql/abq-move-safe.sql
@@ -1,1 +1,37 @@
-asdf
+-- create the crime entitiy --
+CREATE TABLE crime (
+	crimeObjectId    INT UNSIGNED NOT NULL,
+	crimeLocation    VARCHAR(72)  NOT NULL,
+	crimeDescription VARCHAR(255) NOT NULL,
+	crimeDate        DATETIME,
+	UNIQUE (crimeObjectId),
+	PRIMARY KEY (crimeObjectId)
+);
+
+-- create the parks entity --
+CREATE TABLE parks (
+	parkObjectId             INT UNSIGNED NOT NULL,
+	parkName                 VARCHAR(60)  NOT NULL,
+	parkLitTennisCourts      INT UNSIGNED,
+	parkAcres                INT UNSIGNED,
+	parkUnlitTennisCourts    INT UNSIGNED,
+	parkPlayAreas            INT UNSIGNED,
+	parkFullBasketballCourts INT UNSIGNED,
+	parkHalfBasketballCourts INT UNSIGNED,
+	parkHalfBasketballCourts INT UNSIGNED,
+	parkSoccerFields         INT UNSIGNED,
+	parkLitSoftballFields    INT UNSIGNED,
+	parkUnlitSoftballFields  INT UNSIGNED,
+	parkYouthBallFields      INT UNSIGNED,
+	parkIndoorPools          INT UNSIGNED,
+	parkOutdoorPools         INT UNSIGNED,
+	parkHorseshoePits        INT UNSIGNED,
+	parkVolleyballCourts     INT UNSIGNED,
+	parkBackstops            INT UNSIGNED,
+	parkPicnicTables         INT UNSIGNED,
+	parkShadeStructures      INT UNSIGNED,
+	parkParkingSpaces        INT UNSIGNED,
+	parkJoggingPaths         INT UNSIGNED,
+	UNIQUE (parkObjectId),
+	PRIMARY KEY (parkObjectId)
+);
