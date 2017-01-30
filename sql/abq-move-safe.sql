ad9bf24f01f5f8816bc4976ad5e2027455483a79
Brett Gilbert <bgilb13@gmail.com>
2017-01-30 14:50:41 -0700

    changed DDL to new project direction https://app.asana.com/0/256424297438821/256424297438825

--- sql/abq-move-safe.sql
+++ sql/abq-move-safe.sql
@@ -2,35 +2,39 @@
 CREATE TABLE crime (
 	crimeObjectId    INT UNSIGNED NOT NULL,
 	crimeLocation    VARCHAR(72)  NOT NULL,
+	crimeGeometry    POINT        NOT NULL,
 	crimeDescription VARCHAR(255) NOT NULL,
 	crimeDate        DATETIME,
 	UNIQUE (crimeObjectId),
 	PRIMARY KEY (crimeObjectId)
 );
 
--- create the parks entity --
-CREATE TABLE parks (
-	parkObjectId             INT UNSIGNED NOT NULL,
-	parkName                 VARCHAR(60)  NOT NULL,
-	parkLitTennisCourts      INT UNSIGNED  DEFAULT 0,
-	parkAcres                DECIMAL(8, 4) DEFAULT 0.0,
-	parkUnlitTennisCourts    INT UNSIGNED  DEFAULT 0,
-	parkPlayAreas            INT UNSIGNED  DEFAULT 0,
-	parkFullBasketballCourts INT UNSIGNED  DEFAULT 0,
-	parkHalfBasketballCourts INT UNSIGNED  DEFAULT 0,
-	parkSoccerFields         INT UNSIGNED  DEFAULT 0,
-	parkLitSoftballFields    INT UNSIGNED  DEFAULT 0,
-	parkUnlitSoftballFields  INT UNSIGNED  DEFAULT 0,
-	parkYouthBallFields      INT UNSIGNED  DEFAULT 0,
-	parkIndoorPools          INT UNSIGNED  DEFAULT 0,
-	parkOutdoorPools         INT UNSIGNED  DEFAULT 0,
-	parkHorseshoePits        INT UNSIGNED  DEFAULT 0,
-	parkVolleyballCourts     INT UNSIGNED  DEFAULT 0,
-	parkBackstops            INT UNSIGNED  DEFAULT 0,
-	parkPicnicTables         INT UNSIGNED  DEFAULT 0,
-	parkShadeStructures      INT UNSIGNED  DEFAULT 0,
-	parkParkingSpaces        INT UNSIGNED  DEFAULT 0,
-	parkJoggingPaths         INT UNSIGNED  DEFAULT 0,
+-- create the park entity --
+CREATE TABLE park (
+	parkObjectId  INT UNSIGNED NOT NULL,
+	parkName      VARCHAR(60)  NOT NULL,
+	parkGeometry  POINT        NOT NULL,
+	parkDeveloped BOOLEAN,
 	UNIQUE (parkObjectId),
 	PRIMARY KEY (parkObjectId)
+);
+
+-- creates the amenity entity --
+CREATE TABLE amenity (
+	amenityId       INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	amenityCityName VARCHAR(32)                 NOT NULL,
+	amenityName     VARCHAR(32)                 NOT NULL,
+	UNIQUE (amenityId),
+	PRIMARY KEY (amenityId)
+);
+
+-- creates the amenityPark weak attribute --
+CREATE TABLE amenityPark (
+	amenityParkAmenityId    INT UNSIGNED NOT NULL,
+	amenityParkParkObjectId INT UNSIGNED NOT NULL,
+	amenityParkValue        INT UNSIGNED,
+	INDEX (amenityParkAmenityId),
+	FOREIGN KEY (amenityParkAmenityId) REFERENCES amenity (amenityId),
+	INDEX (amenityParkParkObjectId),
+	FOREIGN KEY (amenityParkParkObjectId) REFERENCES park (parkObjectId)
 );
