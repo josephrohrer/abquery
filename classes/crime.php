712f0dd495ae2d557665b6815431b079327305f3
Brett Gilbert <bgilb13@gmail.com>
2017-01-31 08:18:21 -0700

    added classes directory and crime.php https://app.asana.com/0/256424297438821/256424297438825

---
+++ classes/crime.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Classes for the crime entity
+ *
+ * These are the classes for the crime entity including the accessor and mutator methods
+ *
+ * @author bgilbert9@cnm.edu
+ **/
+class Crime implements \JsonSerializable {
+	/**
+	 * id for each individual crime as provided by the city data
+	 */
+	private $crimeObjectId
+	/**
+	 *
+	 **/
+}
