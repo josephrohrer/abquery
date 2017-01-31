c5cddfa6fb837593c3d32a1ece8ef968745d0451
Joseph Rohrer <josephrohrer@me.com>
2017-01-31 10:50:35 -0700

    Created Park PHP class.

---
+++ php/classes/Park.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Classes for the Park entity
+ *
+ * These are the classes for the park entity including the accessor and mutator methods
+ *
+ * @author Joseph Rohrer <jrohrer@cnm.edu>
+ *
+ **/
+class Park implements \JsonSerializable {
+	/**
+	 *
+	 **/
+	private $parkId;
+	/**
+	 *
+	 **/
+	private $parkName;
+	/**
+	 *
+	 **/
+	private $parkGeometry;
+	/**
+	 *
+	 **/
+	private $parkDeveloped;
+	/**
+	 *
+	 **/
+}
