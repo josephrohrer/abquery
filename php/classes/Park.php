3931b515fffa64d57620a55f925bf9bea75e28bb
Joseph Rohrer <josephrohrer@me.com>
2017-01-31 11:42:07 -0700

    Updated Park PHP class.
    https://app.asana.com/0/256424297438821/258723822752781

--- php/classes/Park.php
+++ php/classes/Park.php
@@ -30,4 +30,78 @@ class Park implements \JsonSerializable {
 	 **/
 	private $parkDeveloped;
 
+	/**
+	 * constructor for the Park class
+	 *
+	 * @param int $newParkId
+	 * @param string $newParkName
+	 * @param point $newParkGeometry
+	 * @param boolean $newParkDeveloped
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+	public function __construct() {
+		try {
+			$this->setParkId($newParkId)
+			$this->setParkName($newParkName)
+			$this->setParkGeometry($newParkGeometry)
+			$this->setParkDeveloped($newParkDeveloped)
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for park id
+	 *
+	 **/
+
+	/**
+	 * mutator method for park id
+	 *
+	 */
+
+	/**
+	 * accessor method for park name
+	 *
+	 **/
+
+	/**
+	 * mutator method for park name
+	 *
+	 */
+
+	/**
+	 * accessor method for park geometry
+	 *
+	 **/
+
+	/**
+	 * mutator method for park geometry
+	 *
+	 */
+
+	/**
+	 * accessor method for park developed
+	 *
+	 **/
+
+	/**
+	 * mutator method for park developed
+	 *
+	 */
+
+	public function jsonSerialize() {
+		// TODO: Implement jsonSerialize() method.
+	}
+
+
 }
