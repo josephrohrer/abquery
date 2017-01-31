097503a6cb61830773e91599043d97d985443d7e
Joseph Rohrer <josephrohrer@me.com>
2017-01-31 11:13:47 -0700

    Updated Park PHP class.
    https://app.asana.com/0/256424297438821/258723822752781

--- php/classes/Park.php
+++ php/classes/Park.php
@@ -10,22 +10,24 @@
  **/
 class Park implements \JsonSerializable {
 	/**
-	 *
+	 * id for each park as provided by ABQ city data; this is the primary key
+	 * @var int $parkId
 	 **/
 	private $parkId;
 	/**
-	 *
+	 * name of each park as provided by ABQ city data
+	 * @var string $parkName
 	 **/
 	private $parkName;
 	/**
-	 *
+	 * spatial coordinates that are used to put a center of the park on a map
+	 * @var point $parkGeometry
 	 **/
 	private $parkGeometry;
 	/**
-	 *
+	 * boolean that will determine if park area is developed or undeveloped
+	 * @var boolean $parkDeveloped
 	 **/
 	private $parkDeveloped;
-	/**
-	 *
-	 **/
+
 }
