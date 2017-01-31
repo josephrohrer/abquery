0d601a66b6cc0b79099033ba02b882366b77220d
Brett Gilbert <bgilb13@gmail.com>
2017-01-31 11:15:04 -0700

    added constructor to crime https://app.asana.com/0/256424297438821/258723822752779

--- php/classes/Crime.php
+++ php/classes/Crime.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Classes for the crime entity
  *
@@ -18,7 +19,7 @@ class Crime implements \JsonSerializable {
 	 */
 	private $crimeLocation;
 	/**
-	 * type of crime comitted i.e. robbery, auto theft, etc. this ia a ghost entity
+	 * type of crime committed i.e. robbery, auto theft, etc. this ia a ghost entity
 	 * @var string $crimeDescription
 	 */
 	private $crimeDescription;
@@ -36,32 +37,62 @@ class Crime implements \JsonSerializable {
 
 	/**
 	 * constructor for the crime entity
-	 */
-
-
-	/**
-	 * accessor method for crimeId
-	 */
-
-
-	/**
-	 * mutator for crimeId
-	 */
-
-
-	/**
-	 * accessor for crimeLocation
 	 *
-	 * @return string value of crime location content
+	 * @param int $newCrimeId id of a specific crime
+	 * @param string $newCrimeLocation block-level location that the crime was committed
+	 * @param string $newCrimeDescription the type of crime that was committed
+	 * @param point $newCrimeGeometry coordinates near where the crime was committed
+	 * @param datetime $newCrimeDate date on which the crime was reported
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data violates type hints
+	 * @throws \Exception if some other exception occurs
 	 */
-	public function getCrimeLocation() {
-		return($this->crimeLocation);
+	public function __construct(int $newCrimeId, string $newCrimeLocation, string $newCrimeDescription, point $newCrimeGeometry, datetime $newCrimeDate) {
+		try {
+			$this->setCrimeId($newCrimeId);
+			$this->setCrimeLocation($newCrimeLocation);
+			$this->setCrimeDescription($newCrimeDescription);
+			$this->setCrimeGeometry($newCrimeGeometry);
+			$this->setCrimeDate($newCrimeDate);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 	}
-	/**
-	 * mutator method for crime location
-	 *
-	 * @param string $newCrimeLocation new value of crime location
-	 * @throws \InvalidArgumentException if $newCrimeLocation is insecure
-	 * @throws \RangeException
-	 */
+
+
+/**
+ * accessor method for crimeId
+ */
+
+
+/**
+ * mutator for crimeId
+ */
+
+
+/**
+ * accessor for crimeLocation
+ *
+ * @return string value of crime location content
+ */
+public
+function getCrimeLocation() {
+	return ($this->crimeLocation);
+}
+
+
+/**
+ * mutator method for crime location
+ *
+ * @param string $newCrimeLocation new value of crime location
+ * @throws \InvalidArgumentException if $newCrimeLocation is insecure
+ * @throws \RangeException
+ */
 }
