750f4cf89811a1f793284e4213e6be881e4f8d99
Jennifer Minnich <jminniecc@gmail.com>
2017-02-01 15:59:05 -0700

    commit amenity.php with beginning of function gets amenity by amenityId
    https://app.asana.com/0/256424297438821/258723822752783

--- php/classes/Amenity.php
+++ php/classes/Amenity.php
@@ -236,4 +236,41 @@ class Amenity implements \JsonSerialize {
 		}
 		return($amenities);
 	}
+	/**
+	 * gets the Amenity by amenity id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $amenityId amenity id to search for
+	 * @return Tweet|null Tweet found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getTweetByTweetId(\PDO $pdo, int $tweetId) {
+		// sanitize the tweetId before searching
+		if($tweetId <= 0) {
+			throw(new \PDOException("tweet id is not positive"));
+		}
+
+		// create query template
+		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetId = :tweetId";
+		$statement = $pdo->prepare($query);
+
+		// bind the tweet id to the place holder in the template
+		$parameters = ["tweetId" => $tweetId];
+		$statement->execute($parameters);
+
+		// grab the tweet from mySQL
+		try {
+			$tweet = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($tweet);
+	}
 }
