79867577fbf8f4fba1e236793fa1aa38192816e0
Ben Smith <rios7467@gmail.com>
2017-02-01 09:33:27 -0700

    Added an insert pdo to feature but there is a yellow error from the mySQL aspect, will attempt to remedy. https://app.asana.com/0/256424297438821/256424297438825

--- php/classes/Feature.php
+++ php/classes/Feature.php
@@ -27,12 +27,12 @@ class Feature implements \JsonSerializable {
 	 * constructor for the Feature entity
 	 *
 	 * @param int|null $newFeatureAmenityId id of this amenity feature or null if new amenity feature
-	 *@param int|null $newFeatureParkId id of this park feature or null if new park feature
-	 *@param int|null $newFeatureValue value of this feature or null if new value
-	 *@throws \InvalidArgumentException if the data is not valid format
-	 *@throws \RangeException if the data is out of range value parameter
-	 *@throws \TypeError if the data violates type
-	 *@throws \Exception if any other exception occurs
+	 * @param int|null $newFeatureParkId id of this park feature or null if new park feature
+	 * @param int|null $newFeatureValue value of this feature or null if new value
+	 * @throws \InvalidArgumentException if the data is not valid format
+	 * @throws \RangeException if the data is out of range value parameter
+	 * @throws \TypeError if the data violates type
+	 * @throws \Exception if any other exception occurs
 	 */
 
 	public function __construct(int $newFeatureAmenityId = null, int $newFeatureParkId = null, int $newFeatureValue = null) {
@@ -49,27 +49,29 @@ class Feature implements \JsonSerializable {
 		} catch(\TypeError $typeError) {
 			//rethrow the exception to the caller
 			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
-		}catch (\Exception $exception) {
+		} catch(\Exception $exception) {
 			//rethrow the exception to the caller
 			throw(new \Exception($exception->getMessage(), 0, $exception));
 		}
 	}
+
 	/**
 	 * accessor method for feature amenity id
 	 */
 	public function getFeatureAmenityId() {
-		return($this->featureAmenityId);
+		return ($this->featureAmenityId);
 	}
+
 	/**
 	 *mutator method for feature amenity id
 	 *
-	 *@param int $newFeatureAmenityId new value of amenity feature id
-	 *@throws \RangeException if $newFeatureAmenityId is not positive
-	 *@throws \TypeError if $newFeatureAmenityId is not an integer
+	 * @param int $newFeatureAmenityId new value of amenity feature id
+	 * @throws \RangeException if $newFeatureAmenityId is not positive
+	 * @throws \TypeError if $newFeatureAmenityId is not an integer
 	 */
 	public function setFeatureAmenityId(int $newFeatureAmenityId) {
 		//verify that the amenity id is positive
-		if($newFeatureAmenityId <=0) {
+		if($newFeatureAmenityId <= 0) {
 			throw(new \RangeException("the amenity id is not positive"));
 		}
 		// convert and store the amenity id
@@ -84,6 +86,7 @@ class Feature implements \JsonSerializable {
 	public function getFeatureParkId() {
 		return $this->featureParkId;
 	}
+
 	/**
 	 * mutator method for feature park id
 	 *
@@ -94,17 +97,19 @@ class Feature implements \JsonSerializable {
 	 */
 
 	public function setFeatureParkId(int $newFeatureParkId) {
-		 //verify that the function park id is positive
+		//verify that the function park id is positive
 		if($newFeatureParkId <= 0) {
 			throw(new \RangeException("the function park id is not positive"));
 		}
 	}
+
 	/**
 	 * accessor method for feature value
 	 */
-		public function getFeatureValue() {
-			return $this->featureValue;
+	public function getFeatureValue() {
+		return $this->featureValue;
 	}
+
 	/**
 	 * mutator method for feature value
 	 *
@@ -115,16 +120,43 @@ class Feature implements \JsonSerializable {
 	 */
 	public function setFeatureValue(int $newFeatureValue) {
 		//verify that the function park id is positive
-		if($newFeatureValue <0) {
+		if($newFeatureValue < 0) {
 			throw(new \RangeException("the value is less than zero"));
 		}
 	}
+
+	/**
+	 * inserts this Feature into mySQL
+	 *
+	 * @param \PDO $PDO PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $PDO) {
+
+	//create query template
+$query = "INSERT INTO feature(featureAmenityId, featureParkId, featureValue) VALUES(:featureAmenityId, :featureParkId, :featureValue";
+$statement = $PDO->prepare($query);
+
+//bind the member variables to the place holders in the template
+$parameters = ["featureAmenityId" => $this->featureAmenityId, "featureParkId" => $this->featureParkId, "featureValue" => $this->featureValue];
+$statement->execute($parameters);
+}
 		/**
-		 * formats all of the available variables to JSON readable format.
+		 * formats the state variables to JSON readable format.
+		 *
+		 * @return array making state variables to serialize.
 		 */
 		public function jsonSerialize() {
 			$fields = get_object_vars($this);
 			return($fields);
-		}
+			/**
+			 * inserts this Value into mySQL
+			 *
+			 * @param \PDO $pdo PDO connection object
+			 * @throws \PDOException when mySQL related errors occur
+			 * @throws \TypeError if $pdo is not a PDO connection object
+			 **/
 
+		}
 }
