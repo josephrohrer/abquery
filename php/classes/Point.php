1458bed1b1bb3db8c01606630d742e29569d2530
Brett Gilbert <bgilb13@gmail.com>
2017-02-02 11:54:46 -0700

    completed the point class https://app.asana.com/0/256424297438821/258723822752779

--- php/classes/Point.php
+++ php/classes/Point.php
@@ -14,28 +14,82 @@ class Point implements \JsonSerializable {
 	/**
 	 * @var float $longitude
 	 */
-	private $longitude;
+	private $latitude;
 	/**
 	 * @var float $latitude
 	 */
-	private $latitude;
+	private $longitude;
 
 
 	/**
 	 * constructor for the point entity
 	 *
-	 * @param float $longitude the longitude value of the point
-	 * @param float @latitude the latitude value of the point
+	 * @param float $newLatitude the longitude value of the point
+	 * @param float @newLongitude the latitude value of the point
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception
 	 */
 	public function __construct(float $newLatitude, float $newLongitude) {
+		try {
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
+
+	/**
+	 * accessor method for latitude
+	 *
+	 * @return float value for latitude
+	 */
+	public function getLatitude() {
+		return($this->latitude);
+	}
+
+
+	/**
+	 * mutator method for latitude
+	 *
+	 * @param float $newLatitude new value of Latitude
+	 * @throws \RangeException if $newLatitude is not in the range(-180,180)
+	 */
+	public function setLatitude(float $newLatitude) {
+		if($newLatitude < -180 || $newLatitude > 180)
+			throw(new \RangeException("latitude is not within the range (-180,180)"));
+	}
+
+
+	/**
+	 * accessor method for longitude
+	 *
+	 * @return float value for longitude
+	 */
+	public function getLongitude() {
+		return($this->longitude);
+	}
+
+
+	/**
+	 * mutator method for longitude
+	 *
+	 * @param float $newLongitude new value of longitude
+	 * @throws \RangeException if $newLongitude is not in the range(-90,90)
+	 */
+	public function setLongitude(float $newLongitude) {
+		if($newLongitude < -90 || $newLongitude > 90)
+			throw(new \RangeException("longitude is not within the range (-90,90)"));
 	}
 
 
 	public function jsonSerialize() {
-		// TODO: Implement jsonSerialize() method.
+		return(get_object_vars($this));
 	}
 }
