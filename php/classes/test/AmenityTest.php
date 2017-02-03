888f939298141a27d32432c1823099baee23b245
Jennifer Minnich <jminniecc@gmail.com>
2017-02-03 09:18:44 -0700

    commit amenityTest  doc block
    https://app.asana.com/0/256424297438821/258723822752783

---
+++ php/classes/test/AmenityTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Edu\Cnm\Abquery\Test;
+
+use Edu\Cnm\Abquery\{Amenity};
+
+// grab the project test parameters
+require_once("abqueryTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Amenity class
+ *
+ * This is a complete PHPUnit test of the Amenity class. It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see Tweet
+ * @author Jennifer Quay <jminnich@cnm.edu>
+ **/
+
+class AmenityTest extends AbqueryTest {
+
+}
