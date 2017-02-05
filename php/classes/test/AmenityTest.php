911ae2f4058777fed708374a75b1a1f22ea1f0ed
Jennifer Minnich <jminniecc@gmail.com>
2017-02-04 20:20:57 -0700

    commit amenityTest  function setUp
    https://app.asana.com/0/256424297438821/258723822752783

--- php/classes/test/AmenityTest.php
+++ php/classes/test/AmenityTest.php
@@ -15,9 +15,33 @@ require_once(dirname(__DIR__) . "/classes/autoload.php");
  * This is a complete PHPUnit test of the Amenity class. It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
  *
  * @see Tweet
- * @author Jennifer Quay <jminnich@cnm.edu>
+ * @author Jennifer Quay Minnich <jminnich@cnm.edu>
  **/
 
 class AmenityTest extends AbqueryTest {
+	/**
+	 * name of the Amenity
+	 * @var string $VALID_AMENITYNAME
+	 **/
+	protected $VALID_AMENITYNAME = "PHPUnit test passing";
+	/**
+	 * city name of the Amenity
+	 * @var string $VALID_AMENITYCITYNAME
+	 **/
+	protected $VALID_AMENITYCITYNAME = "PHPUnit test passing";
 
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a Profile to own the test Amenity
+		$this->profile = new Profile(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->profile->insert($this->getPDO());
+
+		// calculate the date (just use the time the unit test was setup...)
+		$this->VALID_AMENITYDATE = new \DateTime();
+	}
 }
