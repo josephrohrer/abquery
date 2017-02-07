f08f2e5327879013b7f9292e978bc9f7f8bee674
Joseph Rohrer <josephrohrer@me.com>
2017-02-07 11:30:35 -0700

    Created Park Test
    https://app.asana.com/0/252717071352356/258723822752782

--- php/classes/test/ParkTest.php
+++ php/classes/test/ParkTest.php
@@ -17,4 +17,63 @@ require_once(dirname(__DIR__) . "autoload.php");
  * @author Joseph Rohrer <jrohrer@cnm.edu>
  **/
 
-class ParkTest extended AbqueryTest
+class ParkTest extends AbqueryTest {
+	/**
+	 * @var string $VALID_PARKNAME
+	 */
+	protected $VALID_PARKNAME = "PHPUnit test passing";
+	protected $VALID_PARKGEOMETRY = "PHPUnit test passing";
+	protected $VALID_PARKDEVELOPED = "PHPUnit test passing";
+
+	/**
+	 * test inserting a valid Park and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidPark() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("park");
+
+		// create a new Park and insert it into mySQL
+		$park = new Park(null, $this->VALID_PARKNAME, $this->VALID_PARKGEOMETRY, $this->VALID_PARKDEVELOPED);
+		$park->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoPark = Park::getParkByParkId($this->getPDO(), $park->getParkId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("park"));
+	}
+
+	/**
+	 * test inserting a Park that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidPark() {
+		// create a Park with a non null amenity id and watch it fail
+		$park = new Park(AbqueryTest::INVALID_KEY, $this->VALID_PARKNAME, $this->VALID_PARKGEOMETRY, $this->VALID_PARKDEVELOPED);
+		$park->insert($this->getPDO());
+	}
+
+	/**
+	 * test grabbing all Parks
+	 **/
+	public function testGetAllValidParks() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("parks");
+
+		// create a new Park and insert to into mySQL
+		$park = new Park(null, $this->VALID_PARKNAME, $this->VALID_PARKGEOMETRY, $this->VALID_PARKDEVELOPED);
+		$park->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Park::getAllParks($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("park"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Abquery\\DataDesign\\Park", $results);
+
+		// grab the result from the array and validate it
+		$pdoPark = $results[0];
+		$this->assertEquals($pdoPark->getParkName(), $this->VALID_PARKNAME);
+		$this->assertEquals($pdoPark->getParkGeometry(), $this->VALID_PARKGEOMETRY);
+		$this->assertEquals($pdoPark->getParkDeveloped(), $this->VALID_PARKDEVELOPED);
+	}
+
+}
