685fc0ed5eac727c8aa61f02436e159813907be5
Joseph Rohrer <josephrohrer@me.com>
2017-02-07 10:46:36 -0700

    Created Park Test
    https://app.asana.com/0/252717071352356/258723822752782

--- php/classes/test/ParkTest.php
+++ php/classes/test/ParkTest.php
@@ -1,10 +1,20 @@
 <?php
 namespace Edu\Cnm\Abquery\Test;
 
-use Edu\Cnm\Abquery\AbqueryTest\{Park};
+use Edu\Cnm\Abquery\Test\{Park};
 
 // grab the project test parameters
 require_once("AbqueryTest.php");
 
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "autoload.php");
+
+/**
+ * Full PHPUnit test for the Park class
+ *
+ * This is a complete PHPUnit test of the Park class. It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ * @see Tweet example
+ * @author Joseph Rohrer <jrohrer@cnm.edu>
+ **/
+
+class ParkTest extended AbqueryTest
