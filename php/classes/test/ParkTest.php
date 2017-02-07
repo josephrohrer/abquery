a6f7882a2da96e8fdacb4978b7652aad457d636a
Joseph Rohrer <josephrohrer@me.com>
2017-02-07 10:37:28 -0700

    Created Park Test

---
+++ php/classes/test/ParkTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Edu\Cnm\Abquery\Test;
+
+use Edu\Cnm\Abquery\AbqueryTest\{Park};
+
+// grab the project test parameters
+require_once("AbqueryTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "autoload.php");
