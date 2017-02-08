1b413e26a09ffc3a1f59a942faa71644926d6240
Joseph Rohrer <josephrohrer@me.com>
2017-02-08 15:06:02 -0700

    Created Point Unit Test file.

---
+++ php/classes/test/PointTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Edu\Cnm\Abquery\Test;
+use Edu\Cnm\Abquery\{Point};
+
+require_once("AbqueryTest.php");
+require_once(dirname(__DIR__) . "/autoload.php");
+
+/**
+ * PHPUnit test for the Point class
+ *
+ **/
+
+class PointTest
