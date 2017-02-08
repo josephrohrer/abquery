566d148c7359a7e747fcbf60654981445ad0ebf2
Joseph Rohrer <josephrohrer@me.com>
2017-02-08 09:44:39 -0700

    Added base html to index page and added logo

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="css/style.css">
+		<title>ABQuery</title>
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+			<img src="images/logo.png" alt="ABQuery Logo">
+			<br>
+			<input type="text" name="search" placeholder="Search..">
+		</main>
+	</body>
+</html>
