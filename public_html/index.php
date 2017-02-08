ab7f7b5ec8d61be3021774793724a456269690de
Joseph Rohrer <josephrohrer@me.com>
2017-02-08 09:55:08 -0700

    Updated index with search bar styling. Updated CSS with search bar styling

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,22 @@
 		<link rel="stylesheet" href="css/style.css">
 		<title>ABQuery</title>
 	</head>
+	<style>
+		input[type=text] {
+			width: 130px;
+			box-sizing: border-box;
+			border: 2px solid #ccc;
+			border-radius: 4px;
+			font-size: 16px;
+			background-color: white;
+			background-image: url('images/searchicon.png');
+			background-position: 10px 10px;
+			background-repeat: no-repeat;
+			padding: 12px 20px 12px 40px;
+			-webkit-transition: width 0.4s ease-in-out;
+			transition: width 0.4s ease-in-out;
+		}
+	</style>
 	<body>
 		<header>
 		</header>
