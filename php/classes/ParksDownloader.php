dadb12f92f217fa3592c62b4f3e13ef752748148
Joseph Rohrer <josephrohrer@me.com>
2017-02-15 16:34:52 -0700

    Added Crime and Parks Downloader.
    https://app.asana.com/0/252717071352356/271758805776129

---
+++ php/classes/ParksDownloader.php
@@ -0,0 +1,14 @@
+
+
+$parkId = "";
+$parkName = "";
+$parkGeometryX = "";
+$parkGeometryY = "";
+$parkDeveloped = "";
+
+$featureAmenityId = 1;
+
+$amenityId = 1;
+$amenityCityName = "";
+$amenityName = "";
+
