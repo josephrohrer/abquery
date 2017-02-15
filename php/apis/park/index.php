8e4896f6f38c62399ed96fdd8603ab73b8d8e73a
Ben Smith <rios7467@gmail.com>
2017-02-15 16:34:47 -0700

    Added part of the park api file. https://app.asana.com/0/252717071352395/271758805776127

---
+++ php/apis/park/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "autoload.php";
+require_once "/lib/xsrf.php";
+require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
+
+use Edu\Cnm\bsmtih\abquery;
+
+/**
+ * api for Park class
+ *
+ * @author Benjamin Smith <bsmtih@cnm.edu>
+ */
+
+
+// Check the session status. If inactive then start the session.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+//Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+// We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
+
+try {
+	//grab the mySQL database connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/abquery-mysql/park.ini");
+
+	//determines which HTTP method needs to be processed and stores the result in $method.
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_x_HTTP_METHOD"]
+}
+
