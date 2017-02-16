80291c4dd4ed0d20975b81ae6ef38a4ba0cf76b6
Jennifer Minnich <jminniecc@gmail.com>
2017-02-16 11:08:18 -0700

    commit apis/amenity w namespace revisions (2)
    https://app.asana.com/0/256424297438821/271758805776124

--- php/apis/amenity/index.php
+++ php/apis/amenity/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../classes/autoloader.php";
+require_once "../classes/autoload.php";
 require_once "../lib/xsrf.php";
 require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
 
@@ -32,6 +32,7 @@ try {
 	//stores the Primary Key ($amenityId) for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
+//FIXME: do we need this ?
 	//Here we check and make sure that we have the Primary Key ($amenityId) for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
