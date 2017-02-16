2b91f623fa06d26d7b2d0f21c65e276087e1a68c
Joseph Rohrer <josephrohrer@me.com>
2017-02-16 11:37:02 -0700

    Started ParksDownloader.php
    https://app.asana.com/0/252717071352356/271758805776129

--- php/classes/ParksDownloader.php
+++ php/classes/ParksDownloader.php
@@ -1,3 +1,55 @@
+<?php
+
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * This class will download parks data from the City of Albuquerque City Data Database.
+ *
+ * @author Joseph Rohrer jrohrer@cnm.edu
+ *
+ **/
+class DataDownloader {
+
+	/**
+	 *
+	 * parks: http://coagisweb.cabq.gov/arcgis/rest/services/public/recreation/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&f=pjson
+	 *
+	 **/
+
+
+
+
+
+}
+
+DataDownloader::readDataJson("http://coagisweb.cabq.gov/arcgis/rest/services/public/recreation/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&f=pjson");
+
+
+"OBJECTID": 276728,
+    "PARKNAME": "AZTEC",
+    "PARKSTATUS": "D",
+    "JURISDICTIcON": "CITY",
+    "ACRES": 5.5388133799999997,
+    "DEVELOPEDACRES": 5.5,
+    "LITTENNISCOURTS": 0,
+    "UNLITTENNISCOURTS": 6,
+    "PLAYAREAS": 2,
+    "FULLBASKETBALLCOURTS": 0,
+    "HALFBASKETBALLCOURTS": 0,
+    "SOCCERFIELDS": 1,
+    "LITSOFTBALLFIELDS": 0,
+    "UNLITSOFTBALLFIELDS": 0,
+    "YOUTHBALLFIELDS": 0,
+    "INDOORPOOLS": 0,
+    "OUTDOORPOOLS": 0,
+    "HORSESHOEPITS": 0,
+    "VOLLEYBALLCOURTS": 0,
+    "BACKSTOPS": 0,
+    "PICNICTABLES": 7,
+    "SHADESTRUCTURES": 1,
+    "PARKINGSPACES": 0,
+    "JOGGINGPATHS": 0,
+
 
 
 $parkId = "";
@@ -7,8 +59,16 @@ $parkGeometryY = "";
 $parkDeveloped = "";
 
 $featureAmenityId = 1;
+$featureParkId =
+$featureValue =
 
 $amenityId = 1;
 $amenityCityName = "";
 $amenityName = "";
+
+[
+	-106.54086466695048,
+	35.120333961168839
+],
+
 
