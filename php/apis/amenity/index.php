c51827c12d0ea660374a526d39edcef7ec34a053
Jennifer Minnich <jminniecc@gmail.com>
2017-02-21 10:10:51 -0700

    commit dirname
    https://app.asana.com/0/256424297438821/271758805776124

--- php/apis/amenity/index.php
+++ php/apis/amenity/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once "../classes/autoload.php";
-require_once "../lib/xsrf.php";
+require_once dirname(__DIR__,3)."../classes/autoload.php";
+require_once require_once dirname(__DIR__,3)."../lib/xsrf.php";
 require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
 
 use Edu\Cnm\Abquery\Amenity;
