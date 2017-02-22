2b75d227bbdb7d015e7a218210f41af61dce6839
Jennifer Minnich <jminniecc@gmail.com>
2017-02-22 09:59:15 -0700

    commit namespace
    https://app.asana.com/0/256424297438821/271758805776124

--- public_html/apis/amenity/index.php
+++ public_html/apis/amenity/index.php
@@ -32,7 +32,6 @@ try {
 	//stores the Primary Key ($amenityId) for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
-//FIXME: do we need this ?
 	//Here we check and make sure that we have the Primary Key ($amenityId) for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
