7fdde6d8d795f01151d7a52a7c0542ca91dd7e9f
Ben Smith <rios7467@gmail.com>
2017-02-28 10:26:26 -0700

    added template file and htaccess

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1><i class="fa fa-microchip"></i> Welcome to the Angular 2 Demo!</h1>
+			<p class="lead orange">This is a bare-bones Angular 2 Front End :D</p>
+		</div>
+	</div>
+</section>
