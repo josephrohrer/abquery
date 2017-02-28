ca60253fa6040733d63d21418d3e879ae98297d5
Jennifer Minnich <jminniecc@gmail.com>
2017-02-28 09:43:45 -0700

    commit app/app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
+	selector: 'abquery',
+
+	// templateUrl path to your public_html/templates directory.
+	templateUrl: './templates/abquery.php'
+})
+
+export class AppComponent {}
