142d4fb3aba3e4665fb2e9b2546acba25cdfa335
Jennifer Minnich <jminniecc@gmail.com>
2017-02-28 16:35:54 -0700

    delete angular :(

--- public_html/templates/home.php
+++
@@ -1,8 +0,0 @@
-<section class="welcome">
-	<div class="container">
-		<div class="jumbotron text-center">
-			<h1><i class="fa fa-microchip"></i> Welcome to ABQuery!</h1>
-			<p class="lead orange">This is a bare-bones Angular 2 Front End :D</p>
-		</div>
-	</div>
-</section>
