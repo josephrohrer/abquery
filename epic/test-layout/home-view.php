0a02d4b0590a1fe42276b33e07aa89e834153e85
Ben Smith <rios7467@gmail.com>
2017-03-05 19:16:42 -0700

    Added closing > for one of the links

--- epic/test-layout/home-view.php
+++ epic/test-layout/home-view.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" href="images/favicon.ico"
+		<link rel="icon" href="images/favicon.ico">
 
 		<title>ABQuery - Home</title>
 
