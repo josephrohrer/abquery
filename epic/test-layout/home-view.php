0a5523faf5337bce2c0fcb2c14c673fe1e053f45
Jennifer Minnich <jminniecc@gmail.com>
2017-03-05 19:04:35 -0700

    commit home view

--- epic/test-layout/home-view.php
+++ epic/test-layout/home-view.php
@@ -66,7 +66,23 @@
 
 
 		<!-- Search bar -->
+		<div class="container">
+			<div class="row">
 
+				<div class="col-sm-6 col-sm-offset-3">
+					<div id="imaginary_container">
+						<div class="input-group stylish-input-group">
+							<input type="text" class="form-control"  placeholder="Search" >
+							<span class="input-group-addon">
+                        <button type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<!-- Footer -->
 		<footer class="footer">
 			<div class="container">
