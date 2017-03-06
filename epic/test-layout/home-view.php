652ad8d69188c0e6389e6a0c9edfe6f6044b09fb
Joseph Rohrer <josephrohrer@me.com>
2017-03-05 19:21:29 -0700

    Fixed title in Home view.

--- epic/test-layout/home-view.php
+++ epic/test-layout/home-view.php
@@ -31,7 +31,7 @@
 				rel="stylesheet"/>
 
 		<!-- Custom CSS -->
-		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 
 		<!-- jQuery -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
