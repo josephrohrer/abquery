6ea2f797ac7576fb20fd4f28bd88af000e89fe59
Jennifer Minnich <jminniecc@gmail.com>
2017-03-05 19:37:35 -0700

    commit about layout

--- epic/test-layout/about-view.php
+++ epic/test-layout/about-view.php
@@ -8,21 +8,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
 		<link rel="icon" href="images/favicon.ico">
+		<title>ABQuery - About Us</title>
 
-		<title>ABQuery</title>
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
-				rel="stylesheet"/>
-
-		<!-- Custom CSS -->
-		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet"
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -31,26 +26,23 @@
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<!-- jQuery -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
-
-		<!-- jQuery Form, Validate, Additional Methods -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js" type="text/javascript"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
 
-		<!-- MY Custom JS -->
-		<script src="js/custom.js" type="text/javascript"></script>
-		<style type="text/css">
-		</style>
 	</head>
-
-
+	<!-- about-page ==================================================== !-->
 
 	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
@@ -61,45 +53,90 @@
 			</div>
 		</nav>
 
-		<!-- Header -->
-		<header>
-			<div class="container">
-
-			</div>
-		</header>
-
-		<section id="about" class="about-section">
+		<!-- Team Section -->
+		<section id="team" class="bg-light-gray">
 			<div class="container">
 				<div class="row">
-					<div class="col-xl-12">
-						<p>ABQuery is a local web platform that can be used to search for relevant information about a specific area in Albuquerque. This could be used for a resident or potential resident who wanted to gather query data relative to a given location.</p>
-						<p>Data sets were provided by the City of Albuquerque’s open data initiative. The initial data sets utilized were Crime Incidents and Parks. Future data could include: restaurants, breweries, dog parks, and/or sex offenders.</p>
-						<p>This project is the final capstone project from a group of students at the Deep Dive Coding Bootcamp, a 10-week web development and programming bootcamp taught at the CNM STEMulus Center in downtown Albuquerque.</p>
+					<div class="col-lg-12 text-center">
+						<h3 class="section-subheading text-muted">About our amazing team.</h3>
+									<div class="col-xl-12">
+										<p>ABQuery is a local web platform that can be used to search for relevant information about a specific area in Albuquerque. This could be used for a resident or potential resident who wanted to gather query data relative to a given location.</p>
+										<p>Data sets were provided by the City of Albuquerque’s open data initiative. The initial data sets utilized were Crime Incidents and Parks. Future data could include: restaurants, breweries, dog parks, and/or sex offenders.</p>
+										<p>This project is the final capstone project from a group of students at the Deep Dive Coding Bootcamp, a 10-week web development and programming bootcamp taught at the CNM STEMulus Center in downtown Albuquerque.</p>
+									</div>
+								</div>
+							</div>
+						</section>	<!-- Team Section -->
+
+		<!-- Team photos -->
+			<section id="photos">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="team-member">
+							<img src="img/team/1.jpg" class="img-responsive img-circle" alt="">
+							<h4>Brett Gilbert</h4>
+							<ul class="list-inline social-buttons">
+								<li><a href="#"><i class="fa fa-github"></i></a>
+								</li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+			</section>
+
+
+					<div class="col-sm-4">
+						<div class="team-member">
+							<img src="img/team/2.jpg" class="img-responsive img-circle" alt="">
+							<h4>Joseph Rohrer</h4>
+							<ul class="list-inline social-buttons">
+								<li><a href="#"><i class="fa fa-github"></i></a>
+								</li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+
+					<div class="col-sm-4">
+						<div class="team-member">
+							<img src="img/team/3.jpg" class="img-responsive img-circle" alt="">
+							<h4>Jennifer Minnich</h4>
+							<ul class="list-inline social-buttons">
+								<li><a href="#"><i class="fa fa-github"></i></a>
+								</li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>		<!-- Team photos -->
+
+
+		<div class="col-sm-4">
+			<div class="team-member">
+				<img src="img/team/3.jpg" class="img-responsive img-circle" alt="">
+				<h4>Benjamin Smith</h4>
+				<ul class="list-inline social-buttons">
+					<li><a href="#"><i class="fa fa-github"></i></a>
+					</li>
+					<li><a href="#"><i class="fa fa-linkedin"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		</div>
+
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 text-center">
+						<p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
 					</div>
 				</div>
 			</div>
 		</section>
 
-		<!-- Map Section -->
-		<section id="map" class="map-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-xl-12">
-					</div>
-				</div>
-			</div>
-
-		</section>
-
-		<!-- Information Section -->
-		<section id="information" class="information-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-xl-12">
-					</div>
-				</div>
-			</div>
-		</section>
 
 		<!-- Footer -->
 		<footer class="footer">
