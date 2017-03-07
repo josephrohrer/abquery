46b381765c9b218b4257daaa5214a1c07a04480e
Brett Gilbert <bgilb13@gmail.com>
2017-03-07 15:24:46 -0700

    "finished about added template" https://app.asana.com/0/256424297438821/271758805776128

---
+++ epic/test-layout/about-view-temp.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="images/favicon.ico">
+
+		<title>ABQuery - About Us</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+	</head>
+
+
+
+
+
+
+	<body class="sfooter">
+
+		<header class="template-header">
+			<div class="col-12 text-center">
+				<img src="images/logosmaller.png">
+			</div>
+		</header>
+
+		<main class="template-main sfooter-content">
+			<!-- About Section -->
+			<section id="about-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-center">
+							<h2 class="section-heading">About This Project</h2><br>
+							<div class="about">
+								<p><strong>ABQuery</strong> is a local web platform that can be used to search for relevant
+									information about a
+									specific area in Albuquerque. This could be used for a resident or potential resident who wanted
+									to gather query data relative to a given location.</p>
+								<p>Data sets were provided by <a href="https://www.cabq.gov/abq-data/" target="_blank">ABQ Data</a>,
+									the City of Albuquerque’s open data initiative. The initial data sets
+									utilized were Crime Incidents and Parks. Future data could include: restaurants, breweries, dog
+									parks, and/or sex offenders.</p>
+								<p>This project is the final capstone project from a group of students at the <a
+										href="https://stemuluscenter.org/deep-dive-fullstack/" target="_blank"> Deep Dive Coding
+										Bootcamp</a>, a 10-week web development and programming bootcamp taught at the <a
+										href="https://stemuluscenter.org/" target="_blank">CNM STEMulus Center</a>
+									in downtown Albuquerque.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Team Section -->
+			<section id="team-section" class="bg-light-gray">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-center">
+							<h2 class="section-heading">Our Team</h2>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-6">
+							<div class="team-member">
+								<img src="images/bgilbert.jpg" class="img-responsive img-circle" alt="">
+								<h4>Brett Gilbert</h4>
+								<ul class="list-inline social-buttons">
+									<li><a href="http://www.github.com/brett-gilbert" target="_blank"><i
+												class="fa fa-github"></i></a>
+									</li>
+									<li><a href="http://www.linkedin.com/in/brett-gilbert" target="_blank"><i
+												class="fa fa-linkedin"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+
+						<div class="col-md-6">
+							<div class="team-member">
+								<img src="images/jrohrer.jpg" class="img-responsive img-circle" alt="">
+								<h4>Joseph Rohrer</h4>
+								<ul class="list-inline social-buttons">
+									<li><a href="http://www.github.com/josephrohrer" target="_blank"><i
+												class="fa fa-github"></i></a>
+									</li>
+									<li><a href="http://www.linkedin.com/in/josephrohrer" target="_blank"><i
+												class="fa fa-linkedin"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-6">
+							<div class="team-member">
+								<img src="images/jminnich.jpg" class="img-responsive img-circle" alt="">
+								<h4>Jennifer Minnich</h4>
+								<ul class="list-inline social-buttons">
+									<li><a href="https://github.com/QUAY17" target="_blank"><i class="fa fa-github"></i></a>
+									</li>
+									<li><a href="https://www.linkedin.com/in/jennifer-quay-minnich/" target="_blank"><i
+												class="fa fa-linkedin"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+
+						<div class="col-md-6">
+							<div class="team-member">
+								<img src="images/bsmtih.jpg" class="img-responsive img-circle" alt="">
+								<h4>Benjamin Smith</h4>
+								<ul class="list-inline social-buttons">
+									<li><a href="https://github.com/bsmith1188"><i class="fa fa-github"></i></a>
+									</li>
+									<li><a href="#"><i class="fa fa-linkedin"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<aside class="links">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-4">
+						</div>
+						<div class="col-xs-2">
+							<a href="https://stemuluscenter.org/deep-dive-fullstack/" target="_blank">
+								<img src="images/icon-ddcb-lg.png" class="img-responsive img-centered"
+									  alt="Deep Dive Coding Bootcamp"></a>
+						</div>
+						<div class="col-xs-2">
+							<a href="https://www.cabq.gov" target="_blank">
+								<img src="images/Medallion_Dome_3_in.png" class="img-responsive img-centered"
+									  alt="City of Albuquerque"></a>
+						</div>
+						<div class="col-xs-4">
+						</div>
+					</div>
+				</div>
+				</div>
+			</aside>
+		</main>
+
+		<footer class="template-footer">
+			<div class="col-12 text-center">
+				<p class="footer-content text-muted">Copyright &copy; ABQuery 2017  |  <a href="about-view.php">About Us</a></p>
+			</div>
+		</footer>
+
+	</body>
+</html>
