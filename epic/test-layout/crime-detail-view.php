9a95354f38c48a524ea0421e8efc1a39be575ed4
Joseph Rohrer <josephrohrer@me.com>
2017-03-08 13:16:47 -0700

    Started Crime detail views and CSS for it.

--- epic/test-layout/crime-detail-view.php
+++ epic/test-layout/crime-detail-view.php
@@ -1,7 +1,101 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mac
- * Date: 3/3/17
- * Time: 12:43 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="images/favicon.ico">
+
+		<title>Insert Title Here</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+	</head>
+
+
+
+
+
+
+	<body class="sfooter">
+
+		<header class="template-header">
+			<div class="col-12 text-center">
+				<img src="images/logosmaller.png">
+			</div>
+		</header>
+
+		<main class="template-main sfooter-content">
+			<div class="container">
+				<div class="row" id="detail-header">
+					<div class="col-sm-1">
+					</div>
+					<div class="col-sm-10">
+						<h1>CRIME INCIDENTS</h1>
+					</div>
+					<div class="col-sm-1">
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-1">
+					</div>
+					<div class="col-sm-10" id="crime-incident">
+						<h2>Burglary/Breaking and Entering</h2>
+						<p>3500 Block Rio Grande Blvd NW<br>
+							03/03/2016</p>
+					</div>
+					<div class="col-sm-1">
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-1">
+					</div>
+					<div class="col-sm-10" id="crime-incident">
+						<h2>Larceny All Other</h2>
+						<p>8200 Block Krim Dr NE<br>
+							03/06/2016</p>
+					</div>
+					<div class="col-sm-1">
+					</div>
+				</div>
+
+			</div>
+		</main>
+
+		<footer class="template-footer">
+			<div class="col-12 text-center">
+				<p class="footer-content text-muted">Copyright &copy; ABQuery 2017  |  <a href="about-view.php">About Us</a></p>
+			</div>
+		</footer>
+
+	</body>
+</html>
