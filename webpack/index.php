ffa0d187cf4bc283f5f8ab2fd450f21a36db4e06
Jennifer Minnich <jminniecc@gmail.com>
2017-03-09 13:40:08 -0700

    commit webpack index.php

--- webpack/index.php
+++ webpack/index.php
@@ -9,11 +9,12 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="images/favicon.ico">
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
 
 		<title>ABQuery</title>
 	</head>
-	<body>
+	<body class="sfooter">
 		<!-- This custom tag much match your Angular @Component selector name in app/app.component.ts -->
 		<abquery>Loading&hellip;</abquery>
 	</body>
