b1ad9d0623e2a8f8ba83c6588344014f907d49cd
Jennifer Minnich <jminniecc@gmail.com>
2017-03-10 09:59:36 -0700

    commit about-view

---
+++ public_html/templates/about.php
@@ -0,0 +1,121 @@
+<search-header></search-header>
+<!-- About Section -->
+<section id="about-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2 class="section-heading">About This Project</h2><br>
+				<div class="about">
+					<p><strong>ABQuery</strong> is a local web platform that can be used to search for relevant
+						information about a
+						specific area in Albuquerque. This could be used for a resident or potential resident who wanted
+						to gather query data relative to a given location.</p>
+					<p>Data sets were provided by <a href="https://www.cabq.gov/abq-data/" target="_blank">ABQ Data</a>,
+						the City of Albuquerque’s open data initiative. The initial data sets
+						utilized were Crime Incidents and Parks. Future data could include: restaurants, breweries, dog
+						parks, and/or sex offenders.</p>
+					<p>This project is the final capstone project from a group of students at the <a
+							href="https://stemuluscenter.org/deep-dive-fullstack/" target="_blank"> Deep Dive Coding
+							Bootcamp</a>, a 10-week web development and programming bootcamp taught at the <a
+							href="https://stemuluscenter.org/" target="_blank">CNM STEMulus Center</a>
+						in downtown Albuquerque.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Team Section -->
+<section id="team-section" class="bg-light-gray">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2 class="section-heading">Our Team</h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="images/bgilbert.jpg" class="img-responsive img-circle" alt="">
+					<h4>Brett Gilbert</h4>
+					<ul class="list-inline social-buttons">
+						<li><a href="http://www.github.com/brett-gilbert" target="_blank"><i
+									class="fa fa-github"></i></a>
+						</li>
+						<li><a href="http://www.linkedin.com/in/brett-gilbert" target="_blank"><i
+									class="fa fa-linkedin"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="images/jrohrer.jpg" class="img-responsive img-circle" alt="">
+					<h4>Joseph Rohrer</h4>
+					<ul class="list-inline social-buttons">
+						<li><a href="http://www.github.com/josephrohrer" target="_blank"><i
+									class="fa fa-github"></i></a>
+						</li>
+						<li><a href="http://www.linkedin.com/in/josephrohrer" target="_blank"><i
+									class="fa fa-linkedin"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="images/jminnich.jpg" class="img-responsive img-circle" alt="">
+					<h4>Jennifer Minnich</h4>
+					<ul class="list-inline social-buttons">
+						<li><a href="https://github.com/QUAY17" target="_blank"><i class="fa fa-github"></i></a>
+						</li>
+						<li><a href="https://www.linkedin.com/in/jennifer-quay-minnich/" target="_blank"><i
+									class="fa fa-linkedin"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="images/bsmtih.jpg" class="img-responsive img-circle" alt="">
+					<h4>Benjamin Smith</h4>
+					<ul class="list-inline social-buttons">
+						<li><a href="#"><i class="fa fa-github"></i></a>
+						</li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<aside class="links">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4">
+			</div>
+			<div class="col-xs-2">
+				<a href="https://stemuluscenter.org/deep-dive-fullstack/" target="_blank">
+					<img src="images/icon-ddcb-lg.png" class="img-responsive img-centered"
+						  alt="Deep Dive Coding Bootcamp"></a>
+			</div>
+			<div class="col-xs-2">
+				<a href="https://www.cabq.gov" target="_blank">
+					<img src="images/Medallion_Dome_3_in.png" class="img-responsive img-centered"
+						  alt="City of Albuquerque"></a>
+			</div>
+			<div class="col-xs-4">
+			</div>
+		</div>
+	</div>
+</aside>
