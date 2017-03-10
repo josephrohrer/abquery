c390b8503eebae6ce60f2e2b6e7545cfecbd2050
Joseph Rohrer <josephrohrer@me.com>
2017-03-10 13:38:45 -0700

    Added homebody id to a div so that mobile.jpg shows up in the background.

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,21 +1,25 @@
-<div class=" container logo-search">
-	<div class="logo text-center">
-		<img class="logo-resize" src="images/abqueryname.png" alt="abquery logo">
-	</div>
+<div id="homebody">
+	<div class=" container logo-search">
+		<div class="logo text-center">
+			<img class="logo-resize" src="images/abqueryname.png" alt="abquery logo">
+		</div>
 
-	<form class="col-sm-8 col-sm-offset-2">
-		<div class="form-group">
-			<label for="searchAddress" class="sr-only">Enter an Albuquerque Address</label>
-			<div class="input-group">
-				<input id="searchAddress" name="searchAddress" type="text" class="form-control input-lg" placeholder="Enter an ABQ address"/>
-				<div class="input-group-btn">
-					<button class="btn btn-default btn-lg" type="button">
-						<i class="glyphicon glyphicon-search"></i>
-					</button>
+
+		<form class="col-sm-8 col-sm-offset-2">
+			<div class="form-group">
+				<label for="searchAddress" class="sr-only">Enter an Albuquerque Address</label>
+				<div class="input-group">
+					<input id="searchAddress" name="searchAddress" type="text" class="form-control input-lg"
+							 placeholder="Enter an ABQ address"/>
+					<div class="input-group-btn">
+						<button class="btn btn-default btn-lg" type="button">
+							<i class="glyphicon glyphicon-search"></i>
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	</form>
+		</form>
+	</div>
 </div>
 
 <!-- consider wrapping video tag in a div with class of hidden-xs -->
