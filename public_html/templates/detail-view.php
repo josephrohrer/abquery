052f6b74db812c420b13a61d31b4c3c9594467f8
Joseph Rohrer <josephrohrer@me.com>
2017-03-13 15:31:24 -0600

    Created detail view with header and parks/crime dropdown selector.

--- public_html/templates/detail-view.php
+++ public_html/templates/detail-view.php
@@ -1,38 +1,14 @@
-<search-header></search-header>
-<!--  list of crimes ================================================= !-->
 
-<div class="container-crime-desktop" data-spy="scroll" data-target="#myScrollspy" data-offset="20">
-	<div class="col-lg-6 col-lg-6-detail-view">
-		<div class="demo-card">
-			<div class="card-header">
-				CRIME INCIDENTS
-			</div>
+<header class="template-header">
+	<div class="col-12 text-center">
+		<img src="images/logosmaller.png">
+	</div>
+</header>
 
-			<div class="card-block">
-				<h4 class="card-title">Breaking and Entering</h4>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">03/03/2016</li>
-					<li class="list-group-item">3500 Block Rio Grande Blvd NW</li>
-				</ul>
-			</div>
+<div class="dropdown dropdown-group">
+	<select class="form-control" id="sel1">
+		<option>Parks</option>
+		<option>Crime</option>
+	</select>
+</div>
 
-			<div class="card-block">
-				<a href="https://bootcamp-coders.cnm.edu/~jminnich/abquery/epic/test-layout/mapjs-view.php"
-					class="btn btn-primary">Map</a>
-			</div>
-			<div class="card-footer text-muted">
-				Abquery: List of Crime Incidents
-			</div>
-		</div><!--card-->
-	</div><!--col-lg-6-->
-</div><!--container-->
-
-<!-- map ==================================================== !-->
-
-<section class="map-section">
-	<div id="map" class="container-fluid">
-		<div class="col-lg-6">
-			<query-map></query-map>
-		</div><!--col-lg-6-->
-	</div><!--container-fluid-->
-</section><!--map-->
