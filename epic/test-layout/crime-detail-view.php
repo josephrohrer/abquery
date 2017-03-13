eba3cba039f9a3c64e26f1c0858863c23b0a85c8
Joseph Rohrer <josephrohrer@me.com>
2017-03-13 11:18:15 -0600

    Updated crime detail view to be sleek.

--- epic/test-layout/crime-detail-view.php
+++ epic/test-layout/crime-detail-view.php
@@ -52,81 +52,117 @@
 
 		<main class="template-main sfooter-content">
 
-		<!--  list of crimes ================================================= !-->
+			<!--  list of crimes ================================================= !-->
 
-	<div class="container-crime">
+			<div class="container-crime">
 
-		<div class="demo-card">
-			<div class="card-header">
-				CRIME INCIDENTS
-			</div>
+				<div class="text-center">
+					<h2>CRIME INCIDENTS</h2>
+				</div>
 
-			<div class="card-block">
-				<h4 class="card-title">Breaking and Entering</h4>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">03/03/2016</li>
-					<li class="list-group-item">3500 Block Rio Grande Blvd NW</li>
-				</ul>
-			</div>
+				<div>
+					<h4>Filters</h4>
+				</div>
 
-			<div class="card-block">
-				<a href="https://bootcamp-coders.cnm.edu/~jminnich/abquery/epic/test-layout/mapjs-view.php" class="btn btn-primary">Map</a>
-			</div>
-			<div class="card-footer text-muted">
-				Abquery: List of Crime Incidents
-			</div>
-		</div><!--card-->
+				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingOne">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
+									aria-expanded="true" aria-controls="collapseOne">
+									Type
+								</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+							<div class="panel-body">
+								Drop down with a list of crime types
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+									Distance
+								</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							<div class="panel-body">
+								Guided up to 50 miles
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+									Date Range
+								</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel"
+							  aria-labelledby="headingThree">
+							<div class="panel-body">
+								Two boxes with one that defaults to recent date
+							</div>
+						</div>
+					</div>
+				</div>
 
+				<!----CRIME CARD----->
+				<div class="demo-card bg-light-gray">
+						<div class="row">
+							<div class="col-xs-8">
+								<h4>AGGRAVATED ASSAULT</h4>
+							</div>
+							<div class="col-xs-4 crime-right">
+								3.4 mi.
+								<a href="#"><img src="images/mappoint.png" alt="Point"> </a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-8">
+								400 BLOCK CHACOMA PL SW
+							</div>
+							<div class="col-xs-4 crime-right">
+								03/16/17
+							</div>
+						</div>
+				</div><!--CRIME CARD END-->
 
-		<div class="demo-card">
-			<div class="card-header">
-				CRIME INCIDENTS
-			</div>
+				<!----CRIME CARD----->
+				<div class="demo-card bg-light-gray">
+					<div class="row">
+						<div class="col-xs-8">
+							<h4>VANDALISM, MALICIOUS MISCHIEF, GRAFFITI</h4>
+						</div>
+						<div class="col-xs-4 crime-right">
+							3.4 mi.
+							<a href="#"><img src="images/mappoint.png" alt="Point"> </a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-8">
+							400 BLOCK CHACOMA PL SW
+						</div>
+						<div class="col-xs-4 crime-right">
+							03/16/17
+						</div>
+					</div>
+				</div><!--CRIME CARD END-->
 
-			<div class="card-block">
-				<h4 class="card-title">Larceny</h4>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">03/06/2016</li>
-					<li class="list-group-item">8200 Block Krim Dr NE</li>
-				</ul>
-			</div>
+			</div><!--container-->
 
-			<div class="card-block">
-				<a href="https://bootcamp-coders.cnm.edu/~jminnich/abquery/epic/test-layout/mapjs-view.php" class="btn btn-primary">Map</a>
-			</div>
-			<div class="card-footer text-muted">
-				Abquery: List of Crime Incidents
-			</div>
-		</div><!--card-->
-
-		<div class="demo-card">
-			<div class="card-header">
-				CRIME INCIDENTS
-			</div>
-
-			<div class="card-block">
-				<h4 class="card-title">Malicious Mischief, Graffiti</h4>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">03/01/2016</li>
-					<li class="list-group-item">5300 Block San Mateo Blvd NE</li>
-				</ul>
-			</div>
-
-			<div class="card-block">
-				<a href="https://bootcamp-coders.cnm.edu/~jminnich/abquery/epic/test-layout/mapjs-view.php" class="btn btn-primary">Map</a>
-			</div>
-			<div class="card-footer text-muted">
-				Abquery: List of Crime Incidents
-			</div>
-		</div><!--card-->
-
-	</div><!--container-->
-
-	</main>
+		</main>
 
 		<footer class="template-footer">
 			<div class="col-12 text-center">
-				<p class="footer-content text-muted">Copyright &copy; ABQuery 2017  |  <a href="about-view.php">About Us</a></p>
+				<p class="footer-content text-muted">Copyright &copy; ABQuery 2017 | <a href="about-view.php">About Us</a>
+				</p>
 			</div>
 		</footer>
 
