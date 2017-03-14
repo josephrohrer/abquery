0d17cddc1616fa88754d04cbf464d3cd10b91686
Brett Gilbert <bgilb13@gmail.com>
2017-03-14 13:25:13 -0600

    added crimeview

---
+++ app/components/crimeview-component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+templateUrl: "./templates/crime-view.php"
+})
+
+export class CrimeViewComponent {
+
+}
---
+++ public_html/templates/crime-view.php
@@ -0,0 +1,7 @@
+<header class="template-header">
+	<div class="col-12 text-center">
+		<a routerLink=""><img src="images/logosmaller.png"></a>
+	</div>
+</header>
+
+<crime></crime>
