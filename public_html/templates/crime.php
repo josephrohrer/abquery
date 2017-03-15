4036d49ea0dc07c07e50006800c4a0137a52c84e
Jennifer Minnich <jminniecc@gmail.com>
2017-03-15 10:46:01 -0600

    commit crime with type filters

--- public_html/templates/crime.php
+++ public_html/templates/crime.php
@@ -19,9 +19,133 @@
 					</a>
 				</h4>
 			</div>
+
+
 			<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-				<div class="panel-body">
-					Drop down with a list of crime types
+				<div class="panel-body amenities-select">
+					<div class="row">
+						<div class="col-xs-6">
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Aggravated Assault</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Arson</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Auto Burglary</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Auto Theft</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Burglary</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Commercial Burglary</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Counterfeiting/ Forgery</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Credit Card Fraud</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Disorderly Conduct</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Disturbance</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Driving While Intoxicated</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Drug/Narcotics Violations</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Embezzlement</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Misappropriation</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Extortion/ Black Mail</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Fraud</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Impersonation</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Intimindation</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Justifiable Homicide</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Larceny</label>
+							</div>
+						</div>
+						<div class="col-xs-6">
+							<div class="checkbox">
+								<label><input type="checkbox" value="">LIQ Laws-Sell</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Loud Party</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Malicious Mischief</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Murder / Nonnegligent Manslaughter</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Pick Pocketing</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Possession of Drug Paraphernalia </label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Prostitution</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Purse Snatching</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Robbery</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Shooting</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Shoplifting</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Simple Assault/ Battery</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Theft From Motor Vehicle</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Theft of Motor Vehicle Part or Accessory</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Vagrancy</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Vandalism</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Weapons Violation</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Welfare Fraud</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="">Worthless Checks</label>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
